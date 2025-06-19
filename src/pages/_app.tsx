d92ce1f6d314c5ad88656e6b9601f1412d530fb1
Dylorena <dilorenasantos@gmail.com>
2025-06-19 14:03:21 -0300

    feat: add google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 // Esse componente fica por volta da aplicação então o que for fixo da página vai nele
+import { useEffect } from 'react';
 import '../styles/global.css';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: 'pageview',
+    page: window.location.pathname,
+  });
+}, []);
   return (
     <Component {...pageProps} />
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,39 @@
 // Esse arquivo vai permitir agir diretamente sobre o html principal, é colocado no documents porque ele carrega uma única vez
 // pois o _app tem um processamento a mais.
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="favicon.png" type="image/png"/>
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani&display=swap"
+            rel="stylesheet"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M8VCZLPP');
+              `,
+            }}
+          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M8VCZLPP"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
